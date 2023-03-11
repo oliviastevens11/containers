5f488d3defe922d121212c0d92f14dd706acef7f
oliviastevens11 <oliviamstevens@yahoo.com>
2023-03-11 13:28:30 -0800

    git linter 50000

--- containers/BinaryTree.py
+++ containers/BinaryTree.py
@@ -88,8 +88,8 @@ class BinaryTree():
         elif traversal_type == 'postorder':
             return self.postorder_print(self.root, '')
         else:
-            raise ValueError('Traversal type' + str(traversal_type) +
-                             ' is not supported.')
+            raise ValueError('Traversal type' + str(traversal_type)
+                                              + ' is not supported.')
 
     def preorder_print(self, start, traversal):
         '''
@@ -211,8 +211,8 @@ class BinaryTree():
         return the sum of these three steps
         '''
         if node:
-            return (1 + BinaryTree.__len__helper(node.left)
-                    + BinaryTree.__len__helper(node.right))
+            return (1 + BinaryTree.__len__helper(node.left) +
+                    BinaryTree.__len__helper(node.right))
         else:
             return 0
 
